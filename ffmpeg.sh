0d9a000f015f3004088a94e1435411f721eeadf0
hamm80 <ham803677@gmail.com>
2024-10-23 14:51:39 +0100

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
 wget https://raw.githubusercontent.com/hamm80/my/refs/heads/main/logo.png
-VBR="1500k"
-FPS="20"
-QUAL="veryfast"
+
 RTMP_URL="rtmps://live-api-s.facebook.com:443/rtmp/"
 read -p "YOUR KEY: " CRP
 RTMP_KEY="$CRP"
@@ -12,7 +10,7 @@ VIDEO_SOURCE="$CRP"
 
 while true; do
 
-ffmpeg -re -i "$VIDEO_SOURCE" -re -i logo.png  -filter_complex "[1:v]scale=60:60[logo];[0:v][logo]overlay=W-w-50:H-h-900" -c:v libx264 -b:v 2000k -preset veryfast -maxrate 3000k -bufsize 6000k -c:a aac -b:a 128k -f flv $RTMP_URL/$RTMP_KEY 
+ffmpeg -re -i "$VIDEO_SOURCE" -re -i logo.png  -filter_complex "[1:v]scale=60:60[logo];[0:v][logo]overlay=W-w-50:H-h-900" -c:v libx264 -b:v 3000k -preset veryfast -maxrate 3000k -bufsize 6000k -c:a aac -b:a 128k -f flv $RTMP_URL/$RTMP_KEY 
 
 echo "البث الأصلي انقطع، سيتم استخدام المصدر البديل..."
 
